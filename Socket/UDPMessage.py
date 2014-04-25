fee5428e4273163e7815cd68735bb57ec6971ce9
Enrique Ramirez <enrique.ramirez35@gmail.com>
2014-04-25 16:58:38 -0600

    Added Log Writer

--- Socket/UDPMessage.py
+++ Socket/UDPMessage.py
@@ -4,7 +4,7 @@ import socket
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 4580
-MESSAGE = "!200001010123401234#"
+MESSAGE = "!200 001 010 123 401 234#"
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
--- Socket/UDPServer.py
+++ Socket/UDPServer.py
@@ -1,33 +1,38 @@
 __author__ = 'luishoracio'
 from socket import socket, AF_INET, SOCK_DGRAM
 from models import MessageProcessor
+from Utils import FileWriter
 import sys
 
 PORT = 4580
 
-def sobek_server(address):
-    try:
-        sock = socket(AF_INET, SOCK_DGRAM)
-        print 'Socket created'
-    except socket.error, msg:
-        print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-        sys.exit()
 
-    sock.bind(address)
-    print('Connected to port ' + str(PORT))
-    while True:
-        print('Waiting for data')
-        msg, addr = sock.recvfrom(1024)
+def sobek_server (address):
+     try:
+          sock = socket (AF_INET, SOCK_DGRAM)
+          print 'Socket created'
+     except socket.error, msg:
+          print 'Failed to create socket. Error Code : ' + str (msg[0]) + ' Message ' + msg[1]
+          sys.exit ()
 
-        if not msg:
-            print('No data received')
-            break
+     sock.bind (address)
+     print('Connected to port ' + str (PORT))
+     while True:
+          print('Waiting for data')
+          msg, addr = sock.recvfrom (1024)
 
-        print('Got message from', addr)
+          if not msg:
+               print('No data received')
+               break
 
-        sock.sendto('OK', addr)
-        MessageProcessor.process_message(msg)
+          print('Got message from', addr)
+
+          sock.sendto ('OK', addr)
+
+          #TODO: fix issues with missing comma
+          FileWriter.writeToFile (msg)
+          #MessageProcessor.process_message(msg)
 
 
 if __name__ == '__main__':
-    sobek_server(('', PORT))
+     sobek_server (('', PORT))

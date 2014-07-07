982508aaab2b8c61967956f870a83c3ad0e1914c
Enrique Ramirez <enrique.ramirez35@gmail.com>
2014-07-07 09:30:57 -0600

    Pointing to real DB

--- Water_Automation/Water_Automation/settings.py
+++ Water_Automation/Water_Automation/settings.py
@@ -13,12 +13,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'Riego.DB',  # Or path to database file if using sqlite3.
+        'NAME': 'Water_Automation',  # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'Water_Automation_DBA',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',  # Set to empty string for default.
     }

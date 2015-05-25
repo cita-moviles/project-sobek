bc3d45046d22e313b6850f56367525226f449a09
Administrator <administrator@riego.(none)>
2015-05-25 12:38:39 -0600

    preparing to merge

--- Water_Automation/Water_Automation/settings.py
+++ Water_Automation/Water_Automation/settings.py
@@ -168,7 +168,4 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',),
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer'
-   ) 
 }

ec3e859bacbe81a1284ddb674a4a3c4bf46cbaaa
Carla08 <dcfc@live.com.mx>
2015-06-19 13:04:42 -0600

    Manual Mode fixed, again (:

--- Water_Automation/FieldApp/static/app.js
+++ Water_Automation/FieldApp/static/app.js
@@ -396,9 +396,9 @@ function weatherCallback(data) {
         radiation_tag.html(radiation);
 
         if (status === 0) {
-            status_tag.html('<p>OK</p>');
+            status_tag.html('<p>Status: OK</p>');
         } else {
-            status_tag.html('<p>Communication Error</p>');
+            status_tag.html('<p>Status: Communication Error</p>');
         }
 
 
